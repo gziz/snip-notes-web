5bbb480d21e29b4e16cedbb027aa635f68459319
gziz <69287011+gziz@users.noreply.github.com>
2023-09-24 20:12:15 -0600

    Add nav urls

--- data.js
+++ data.js
@@ -3,9 +3,11 @@ const tutorials = [
       header: "Creating a Note",
       gifSrc: "resources/gifs/snip-notes-demo.gif",
       steps: [
-        "Click on 'New Note'.",
-        "Type your content.",
-        "Click 'Save'."
+        "Select the code you want to create a note about.",
+        "Open command palette (CMD + SHIFT + P).",
+        'Search for "Snip Notes: Create Note".',
+        'Type your personal note/insight in the box.',
+        'Enter!'
       ]
     },
   ];
