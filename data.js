0189206004d1e954b214c7ec1a7446e4ad1de766
gziz <69287011+gziz@users.noreply.github.com>
2023-09-24 20:00:26 -0600

    Populate html from data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const tutorials = [
+    {
+      header: "Creating a Note",
+      gifSrc: "resources/gifs/snip-notes-demo.gif",
+      steps: [
+        "Click on 'New Note'.",
+        "Type your content.",
+        "Click 'Save'."
+      ]
+    },
+  ];
+  
